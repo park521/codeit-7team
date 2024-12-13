db0e4e7797ba8bf63166a8ca8cdbd68977ab0dd6
park521 <gh10035@naver.com>
2024-12-13 20:26:13 +0900

    app.js 수정, 글로벌css 적용, 뱃지 컴포넌트 추가

--- open-mind/src/App.js
+++ open-mind/src/App.js
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage/Homepage";
 import ButtonTest from "./components/Commons/Buttons/ButtonTest";
 import IndividualFeed from "./pages/IndividualFeed/IndividualFeed";
+import QuestionListPage from "./pages/QuestionListPage/QuestionListPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        {/* 버튼 테스트 페이지 */}
-        <Route path="/buttonTest" element={<ButtonTest />} />
-        {/* 개별 피드 [todo : 이후에 id 속성 적용] */}
-        <Route path="/post" element={<IndividualFeed />} />
+        <Route path="/">
+          <Route index element={<Homepage />} />
+          <Route path="list" element={<QuestionListPage />} />
+          <Route path="post/:subjectSlug" element={<ButtonTest />}>
+            <Route path="answer" element={<IndividualFeed />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
