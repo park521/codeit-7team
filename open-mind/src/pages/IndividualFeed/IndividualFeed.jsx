ff3a38239a19d4135c51c8bbbfa686ae02c747d4
kyungkiK <rudrlalsrl@gmail.com>
2024-12-13 21:00:01 +0900

    전역 css 참조 및 페이지 기본 작업

--- open-mind/src/pages/IndividualFeed/IndividualFeed.jsx
+++ open-mind/src/pages/IndividualFeed/IndividualFeed.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import styles from "./style.module.css";
+import styles from "./feed.module.css";
 import logo from "../../assets/logo/logo.svg";
 import profile from "../../assets/img/profile.svg";
 import LinkItem from "../../components/Commons/Link/LinkItem";
+import messagesIcon from "../../assets/icon/messages-brown.svg";
 
 function IndividualFeed() {
   return (
@@ -28,9 +29,25 @@ function IndividualFeed() {
         </section>
       </header>
       <main className={styles.main}>
-        <section>
-          <section></section>
-          <section></section>
+        <section className={styles.question_num}>
+          <section>
+            <img
+              src={messagesIcon}
+              className={styles.messages_icon}
+              alt="메시지 아이콘"
+            />
+          </section>
+          <section className={styles.question_num_text}>
+            3{/* 변수 들어갈 자리리 */}개의 질문이 있습니다
+          </section>
+        </section>
+        <section className={styles.feed_card}>
+          <section className={styles.answer_boolean}>
+            <section className={styles.answer_boolean_text}>답변 완료</section>
+          </section>
+          <section className={styles.feed_question}>2</section>
+          <section className={styles.feed_answer}>3</section>
+          <section className={styles.feed_favorite}>4</section>
         </section>
       </main>
     </div>
