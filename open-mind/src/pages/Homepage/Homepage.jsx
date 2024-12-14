5602ef2281b96a6426086a8d6d1c4b45b35a7edf
eund00 <leeeunj00@naver.com>
2024-12-14 14:38:37 +0900

    메인페이지 UI 및 svg추가

--- open-mind/src/pages/Homepage/Homepage.jsx
+++ open-mind/src/pages/Homepage/Homepage.jsx
@@ -1,9 +1,22 @@
 import React from "react";
+import "./homepage.css";
+import GoQuestionButton from "../../components/Commons/Buttons/GoQuestionButton";
+import TakeQuestions from "../../components/Commons/Buttons/TakeQuestions";
+
+const go_question_button = "질문하러 가기";
+const take_question_button = "질문 받기";
 
 function Homepage() {
   return (
-    <div>
-      <p>메인 페이지</p>
+    <div className="main">
+      <div className="main__form">
+        <GoQuestionButton button_word={go_question_button}></GoQuestionButton>
+        <h1 className="main__form-logo">OPNE MIND 로고</h1>
+        <div className="main__form-group">
+          <input className="main__form-input" type="text" />
+          <TakeQuestions button_word={take_question_button} />
+        </div>
+      </div>
     </div>
   );
 }
