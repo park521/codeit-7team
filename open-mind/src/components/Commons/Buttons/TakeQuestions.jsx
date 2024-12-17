bcd77bd85d1ac6cc0266f3f2223409a326fdfdfb
eund00 <leeeunj00@naver.com>
2024-12-18 07:26:24 +0900

    메인페이지 api추가 수정 및 moudulel.css변경

--- open-mind/src/components/Commons/Buttons/TakeQuestions.jsx
+++ open-mind/src/components/Commons/Buttons/TakeQuestions.jsx
@@ -1,7 +1,7 @@
 import styles from "./button.module.css";
 
-function TakeQuestions({ button_word }) {
-  return <button className={styles.take_question_button}>{button_word}</button>;
+function TakeQuestions({ button_word, onClick }) {
+  return <button onClick={onClick} className={styles.take_question_button}>{button_word}</button>;
 }
 
 export default TakeQuestions;
--- open-mind/src/pages/Homepage/Homepage.jsx
+++ open-mind/src/pages/Homepage/Homepage.jsx
@@ -1,30 +1,37 @@
 import React, { useState } from "react";
-import "./homepage.css";
+import styles from "./homepage.module.css";
 import GoQuestionButton from "../../components/Commons/Buttons/GoQuestionButton";
 import TakeQuestions from "../../components/Commons/Buttons/TakeQuestions";
 import InputField from "../../components/Commons/InputField/InputField";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { handleCreateFeed } from "../../api/homepage/hompage";
 
 const go_question_button = "질문하러 가기";
 const take_question_button = "질문 받기";
 
 function Homepage() {
   const [name, setName] = useState("");
+  const navigate = useNavigate();
 
   const handleInputChange = (value) => {
     setName(value);
   };
 
+  const handleClickChange = () => {
+    handleCreateFeed(name, navigate);
+  }
+
+
   return (
-    <div className="main">
-      <div className="main__form">
+    <div className={styles.main}>
+      <div className={styles.main__form}>
         <Link to={"/list"}>
           <GoQuestionButton button_word={go_question_button} />
         </Link>
-        <h1 className="main__form-logo">OPNE MIND 로고</h1>
-        <div className="main__form-group">
+        <h1 className={styles.main__form_logo}>OPNE MIND 로고</h1>
+        <div className={styles.main__form_group}>
           <InputField value={name} onChange={handleInputChange} />
-          <TakeQuestions button_word={take_question_button} />
+          <TakeQuestions onClick={handleClickChange} button_word={take_question_button} />
         </div>
       </div>
     </div>
