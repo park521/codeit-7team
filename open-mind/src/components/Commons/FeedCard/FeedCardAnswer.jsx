ba68a796a076471c74f9e5330655cfd8ba28d84b
SeokChan <melee0826@gmail.com>
2024-12-23 03:33:40 +0900

    충돌문제 해결

--- open-mind/src/components/Commons/FeedCard/FeedCardAnswer.jsx
+++ open-mind/src/components/Commons/FeedCard/FeedCardAnswer.jsx
@@ -96,10 +96,7 @@ function FeedCardAnswer({
   putAnswers,
   questionId,
   isEditing,
-<<<<<<< HEAD
   isReject,
-=======
->>>>>>> bf4a22a672fe3b539f2ecc2427652ab13144f844
   initialValues = INITIAL_VALUES,
 }) {
   const [values, setValues] = useState(initialValues);
@@ -195,14 +192,11 @@ function FeedCardAnswer({
               type="submit"
               disabled={!values.content.trim()}
             />
-<<<<<<< HEAD
           </form>
         )}
         {isAnswerPage && answer && isReject && (
           <form>
             <p>답변 거절</p>
-=======
->>>>>>> bf4a22a672fe3b539f2ecc2427652ab13144f844
           </form>
         )}
       </FeedAnswerDetailContainer>
