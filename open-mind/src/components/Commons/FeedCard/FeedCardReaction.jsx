bb6a3c7066f766c99de32783582b602f001a98a2
park521 <gh10035@naver.com>
2024-12-23 20:02:20 +0900

    좋아요 클릭시 색 변경

--- open-mind/src/components/Commons/FeedCard/FeedCardReaction.jsx
+++ open-mind/src/components/Commons/FeedCard/FeedCardReaction.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import { ReactComponent as LikeIcon } from "../../../assets/icon/thumbs-up-gray.svg";
 import { ReactComponent as DisLikeIcon } from "../../../assets/icon/thumbs-down-gray.svg";
 import styled from "styled-components";
@@ -18,13 +19,22 @@ const FeedLike = styled.div`
   gap: 0.375rem;
   font-weight: 500;
   font-size: 0.875rem;
-  color: var(--gray40-color);
+  color: ${(theme) =>
+    theme.isActive ? "var(--blue50-color)" : "var(--gray40-color)"};
+  cursor: pointer;
+
+  path {
+    fill: ${(theme) =>
+      theme.isActive ? "var(--blue50-color)" : "var(--gray40-color)"};
+  }
 `;
 
 const FeedHate = styled(FeedLike)``;
 
 // 피드 좋아요, 싫어요
 function FeedCardReaction({ like, dislike }) {
+  const [isLikeCheck, setIsLikeCheck] = useState(false);
+  const [isDisLikeCheck, setIsDisLikeCheck] = useState(false);
   // 좋아요 개수 0이면 표기 x
   const setLike = () => {
     return like === 0 ? "" : like;
@@ -34,13 +44,21 @@ function FeedCardReaction({ like, dislike }) {
     return dislike === 0 ? "" : dislike;
   };
 
+  const handleLikeClick = () => {
+    setIsLikeCheck(!isLikeCheck);
+  };
+
+  const handleDisLikeClick = () => {
+    setIsDisLikeCheck(!isDisLikeCheck);
+  };
+
   return (
     <FeedFavoriteContainer>
-      <FeedLike>
+      <FeedLike isActive={isLikeCheck} onClick={handleLikeClick}>
         <LikeIcon style={{ width: "1rem", height: "1rem" }} />
         좋아요 {setLike()}
       </FeedLike>
-      <FeedHate>
+      <FeedHate isActive={isDisLikeCheck} onClick={handleDisLikeClick}>
         <DisLikeIcon style={{ width: "1rem", height: "1rem" }} />
         싫어요 {setDisLike()}
       </FeedHate>
